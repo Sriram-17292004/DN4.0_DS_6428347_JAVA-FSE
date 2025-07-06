124cef5f88f81ab367e0594e6b95e3a2f4adb878
Sriram <sriramrayapudi04@gmail.com>
2025-07-06 15:17:18 +0530

    Added Mandatory Hands-On for Week-3

---
+++ Week-3_SpringDataJPAwithSpringBoot&Hibernate/Exercise-1_SpringDataJpa-QuickExample/Code/Country.java
@@ -0,0 +1,24 @@
+package com.cognizant.orm_learn.model;
+
+import jakarta.persistence.*;  // ✅ NOT javax
+
+@Entity
+@Table(name = "country")
+public class Country {
+
+    @Id
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+}
